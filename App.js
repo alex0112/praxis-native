6c841b24c55ea05a0c823555e437946ae187c63a
alex0112 <alarsen0112@gmail.com>
2020-02-17 23:23:57 -0700

    foo

--- App.js
+++ App.js
@@ -1,23 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+	<FlatList
+	  ItemSeparatorComponent={Platform.OS !== 'android' && ({highlighted}) => (
+	      <View style={[style.separator, highlighted && {marginLeft: 0}]} />
+	  )}
+	data={[{title: 'Title Text', key: 'item1'}]}
+	renderItem={({item, separators}) => (
+	    <TouchableHighlight
+	      onPress={() => this._onPress(item)}
+	      onShowUnderlay={separators.highlight}
+	      onHideUnderlay={separators.unhighlight}>
+	      <View style={{backgroundColor: 'white'}}>
+		<Text>{item.title}</Text>
+	      </View>
+	    </TouchableHighlight>
+	)}
+	    />
+
     );
-  }
+
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
+container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    padding: 50,
+    
+},
 });
